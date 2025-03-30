6ca86601f65bf0d821d5e8c2892492affe397e49
Rabah-Muhammed <rabarabi31@gmail.com>
2025-03-30 14:12:51 +0530

    chore:deployment_settings.py file added

--- bookbounty/settings.py
+++ bookbounty/settings.py
@@ -149,7 +149,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = BASE_DIR/'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- bookbounty/wsgi.py
+++ bookbounty/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookbounty.settings')
+settings_module = 'bookbounty.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'bookbounty.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',settings_module)
+
 
 application = get_wsgi_application()
