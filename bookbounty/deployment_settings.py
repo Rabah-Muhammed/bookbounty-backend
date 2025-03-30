2a4519acc27253318ade9bec50726685dce84145
Rabah-Muhammed <rabarabi31@gmail.com>
2025-03-30 18:03:57 +0530

    fix:aws s3 bucket feature added for static and media managment

--- bookbounty/deployment_settings.py
+++ bookbounty/deployment_settings.py
@@ -27,12 +27,34 @@ CORS_ALLOWED_ORIGINS = [
     
 ]
 
+
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'eu-north-1')
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com"
+AWS_S3_FILE_OVERWRITE = False
+
+MEDIA_URL = f"{AWS_S3_CUSTOM_DOMAIN}/"
+
+
+# STORAGES = {
+#     "default":{
+#         "BACKEND" : "django.core.files.storage.FileSystemStorage",
+#     },
+#     "staticfiles" : {
+#         "BACKEND" : "whitenoise.storage.CompressedStaticFilesStorage",
+#     },
+# }
+
+
 STORAGES = {
-    "default":{
-        "BACKEND" : "django.core.files.storage.FileSystemStorage",
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
     },
-    "staticfiles" : {
-        "BACKEND" : "whitenoise.storage.CompressedStaticFilesStorage",
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
