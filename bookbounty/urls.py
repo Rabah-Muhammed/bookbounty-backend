70431238db950746ed95454bbc51180c1cdfef88
Rabah-Muhammed <rabarabi31@gmail.com>
2025-03-31 19:12:28 +0530

    chore:api documentation completed using openapi

--- bookbounty/urls.py
+++ bookbounty/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
