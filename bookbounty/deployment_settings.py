e6ef57ac5b784acef6836c6831dfb5f2f04bc527
Rabah-Muhammed <rabarabi31@gmail.com>
2025-04-09 12:41:39 +0530

    fix:settings.py updated

--- bookbounty/deployment_settings.py
+++ bookbounty/deployment_settings.py
@@ -39,6 +39,9 @@ STORAGES = {
 }
 
 
+
+
+
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
--- bookbounty/settings.py
+++ bookbounty/settings.py
@@ -163,11 +163,12 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=5), 
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),  
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5), 
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=5),  
     "ROTATE_REFRESH_TOKENS": True, 
     "BLACKLIST_AFTER_ROTATION": True,  
     "UPDATE_LAST_LOGIN": True, 
@@ -177,6 +178,3 @@ SIMPLE_JWT = {
 }
 
 
-REST_FRAMEWORK = {
-    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
-}
